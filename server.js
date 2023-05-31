d8a53824467021b7d2e8db751148b14f12e691d3
Odhran7 <russell.odhran@gmail.com>
2023-05-31 18:59:06 +0100

    error fix

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ passport.use(
     {
       clientID: process.env.LINKEDIN_CLIENT_ID,
       clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/linkedin/callback",
+      callbackURL: "http://valumetrics.ai/linkedin/callback",
       scope: ["r_emailaddress", "r_liteprofile"],
       proxy: true,
     },
@@ -159,7 +159,7 @@ passport.use(
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/google/callback",
+  callbackURL: "http://valumetrics.ai/google/callback",
   passReqToCallback: true,
   proxy: true,
 },
